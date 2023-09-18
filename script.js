c11f4f71a2d24b63222b8bac9cdd4e85591ac535
Stephen <stephensouness@outlook.com>
2023-09-18 14:24:33 +0100

    operate function created - calls the add, subtract. multiply, divide functions

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+function operate(op, num1, num2) {
+  if (op === "+") return add(num1, num2);
+  if (op === "-") return subtract(num1, num2);
+  if (op === "*") return multiply(num1, num2);
+  if (op === "/") return divide(num1, num2);
+}
