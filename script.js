4abb16a30b2db4165e3234a781cae1c9bc6f6274
Stephen <stephensouness@outlook.com>
2023-09-18 14:19:48 +0100

    add, subtract, multiply, divide - basic functions added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Jeevy");
+/* Calculator Functions */
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
