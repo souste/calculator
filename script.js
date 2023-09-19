0b878518330e1cd4c95f946d4c0dae991f9190d7
Stephen <stephensouness@outlook.com>
2023-09-19 08:49:32 +0100

    event listener now applied to add clicked number buttons to display console

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const numButtons = document.querySelectorAll(".num-button");
+const display = document.querySelector(".display");
+
 /* Calculator Functions */
 
 function add(a, b) {
@@ -19,6 +22,7 @@ function divide(a, b) {
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
+let displayValue = "";
 
 function operate(op, num1, num2) {
   if (op === "+") return add(num1, num2);
@@ -26,3 +30,14 @@ function operate(op, num1, num2) {
   if (op === "*") return multiply(num1, num2);
   if (op === "/") return divide(num1, num2);
 }
+
+// Instructions are to create functions that populate the display when I click the number buttons - should I put this into named function??
+
+numButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    let clickedNumber = event.target.value;
+    displayValue += clickedNumber;
+    display.innerText = displayValue;
+    console.log(displayValue);
+  });
+});
